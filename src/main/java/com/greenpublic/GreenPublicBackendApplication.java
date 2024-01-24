e81506c585721b0fbfbe6a8f0cb2abd67c7e0ca2
seferovramin7 <seferovramin7@gmail.com>
2024-01-24 10:00:15 +0400

    Initial commit

---
+++ src/main/java/com/greenpublic/GreenPublicBackendApplication.java
@@ -0,0 +1,13 @@
+package com.greenpublic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenPublicBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreenPublicBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/greenpublic/GreenPublicBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenpublic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenPublicBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
